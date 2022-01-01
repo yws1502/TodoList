3d051f11efd2b1cf37bc159e98b18c2aac998a65
Woosang <yws1502@naver.com>
2022-01-02 04:37:28 +0900

    🐛 Fixed background image path

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
 const bgImages = ["3.jpg", "5.jpg", "6.jpg", "7.jpg"]
 const currBg = bgImages[parseInt(Math.random()*bgImages.length)];
 
-document.body.style.backgroundImage = `url(../img/${currBg})`;
+document.body.style.backgroundImage = `url(./img/${currBg})`;
