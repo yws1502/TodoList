05280dcc8caff489b5d824bf6b0b82e0d73db8fc
Woosang <yws1502@naver.com>
2022-01-01 22:45:08 +0900

    🔥 remove background image files

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const bgImages = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"]
+const bgImages = ["3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"]
 const currBg = bgImages[parseInt(Math.random()*bgImages.length)];
 
 document.body.style.backgroundImage = `url(../img/${currBg})`;
