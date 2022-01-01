4fdff8d9dab7071cd58301a2ae94500417734fb9
Woosang <yws1502@naver.com>
2022-01-01 21:32:11 +0900

    ✨ Create painting greeting function

---
+++ js/greeting.js
@@ -0,0 +1,29 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.getElementById("greeting-user");
+
+const HIDDEN_CLASS = "hidden";
+const USER_KEY = "username";
+
+function paintGreeting(username) {
+  greeting.textContent = `Hello ${username} 🖐`;
+  greeting.classList.remove(HIDDEN_CLASS);
+}
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+
+  loginForm.classList.add(HIDDEN_CLASS);
+  localStorage.setItem(USER_KEY, username);
+  paintGreeting(username);
+}
+
+const userName = localStorage.getItem(USER_KEY);
+
+if (userName) {
+  paintGreeting(userName);
+} else {
+  loginForm.classList.remove(HIDDEN_CLASS);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}
